65607c041f084a6ddaff22ada27f39281c6b32da
parshu-96 <parshuvmahindrakar@gmail.com>
2025-05-04 18:38:09 +0530

    Integrated User Route in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,20 @@
-const dotenv = require('dotenv'); // Import dotenv to load environment variables    
+const dotenv = require("dotenv"); // Import dotenv to load environment variables
 dotenv.config(); // Load environment variables from .env file
-const cors = require('cors'); // Import cors for Cross-Origin Resource Sharing
-
-const express=require("express");
+const cors = require("cors"); // Import cors for Cross-Origin Resource Sharing
+const express = require("express");
 const app = express();
+const connectDB = require("./db/db"); // Import the database connection function
+const userRoutes = require("./routes/user.routes"); // Import user routes
+connectDB(); // Call the function to connect to the database
+
 app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+  res.send("Hello World!");
+});
 
-module.exports = app;   
+app.use("/api/v1/user", userRoutes); // Use user routes for API version 1
 
+module.exports = app;
