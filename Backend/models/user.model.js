e002bde491861e474a08b397aee7098ce5a97c89
parshu-96 <parshuvmahindrakar@gmail.com>
2025-05-04 18:35:39 +0530

    Created user Model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
+    },
+    lastName: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    minlength: [5, "Email must be at least 5 characters long"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+});
+
+userSchema.methods.generateAuthToken = async function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  const salt = await bcrypt.genSalt(10); // Generate a salt with 10 rounds
+  const hashedPassword = await bcrypt.hash(password, salt); // Hash the password with the salt
+  return hashedPassword; // Return the hashed password
+};
+
+const userModel = mongoose.model("User", userSchema); // Create a model from the schema
+module.exports = userModel; // Export the model for use in other files
