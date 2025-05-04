d4f66cd1e91ac57fb083750369eb21c8d0c7600e
parshu-96 <parshuvmahindrakar@gmail.com>
2025-05-04 18:36:32 +0530

    Changed server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,10 @@
-
-const http = require('http');
-const app = require('./app');   
+const http = require("http");
+const app = require("./app");
 const server = http.createServer(app); // Create an HTTP server using the Express app
 
-const port=process.env.PORT || 3000; // Set the port to the value from environment variables or default to 3000     
+const port = process.env.PORT || 3000; // Set the port to the value from environment variables or default to 3000
 
-server.listen(port, () => { // Start the server and listen on the specified port
-    console.log(`Server is running on port ${port}`); // Log the server start message
-})
+server.listen(port, () => {
+  // Start the server and listen on the specified port
+  console.log(`Server is running on port ${port}`); // Log the server start message
+});
