b87857a7b6700fe932ec60422c28da5a7e678294
parshu-96 <parshuvmahindrakar@gmail.com>
2025-05-04 18:36:49 +0530

    Added User Service

---
+++ Backend/services/user.service.js
@@ -0,0 +1,21 @@
+const userModel = require("../models/user.model"); // Import the user model
+
+module.exports.createUser = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}) => {
+  if (!firstName || !email || !password) {
+    throw new Error("Please provide all required fields");
+  }
+  const user = userModel.create({
+    fullName: {
+      firstName,
+      lastName,
+    },
+    email,
+    password,
+  });
+  return user;
+};
